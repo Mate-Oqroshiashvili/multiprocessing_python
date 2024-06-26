06ba8046937240c82b5fc16d570ab0b5b5f9fd0d
Mate oqroshiashvili <oqrosha1313@mail.com>
2024-06-26 15:05:15 +0400

    first commit

---
+++ reader.py
@@ -0,0 +1,24 @@
+import time
+from multiprocessing.managers import BaseManager
+
+class SharedQueueManager(BaseManager):
+    pass
+
+SharedQueueManager.register('SharedQueue')
+
+def reader():
+    manager = SharedQueueManager(address=('localhost', 50000), authkey=b'queue')
+    manager.connect()
+    shared_queue = manager.SharedQueue()
+
+    while True:
+        if not shared_queue.empty():
+            message = shared_queue.get()
+            if message:
+                print(f"Consumed: {message}")
+        else:
+            print("No message in the queue")
+        time.sleep(2)  # Reduced sleep time for quicker demonstration
+
+if __name__ == '__main__':
+    reader()
